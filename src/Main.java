b699b9c99331a1bb6c36c3106a738357d85d54d5
Юлия <karievay@list.ru>
2022-11-04 01:07:44 +0500

    Сделано ДЗ

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        // Задача 4
+        System.out.println("Задача 4");
+        var friend = 19;
+        System.out.println(friend);
+        friend *= 2;
+        System.out.println(friend);
+        friend /= 7;
+        System.out.println(friend);
+
+        // Задача 5
+        System.out.println("Задача 5.");
+        var flog = 3.5;
+        System.out.println(flog);
+        flog *= 10;
+        System.out.println(flog);
+        flog /= 3.5;
+        System.out.println(flog);
+        flog += 4;
+        System.out.println(flog);
+
+
+
+
+    }
+}
